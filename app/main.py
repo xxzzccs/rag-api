a580aa0d42ae5b5aa2a5146261ebd9005a4badc5
= <=>
2026-09-22 15:46:20 +1000

    Created an app directory and moved all python files to app directory

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from db_config import client, collection
 
-from build_knowledge_base import build_knowledge_base
+from app.build_knowledge_base import build_knowledge_base
 from document import Document
 
 import rag
